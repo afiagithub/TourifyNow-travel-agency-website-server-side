8aced74f0a1c4d0db70fce520ac273d2725c0854
Afia Rahman <rahmanmou26@gmail.com>
2024-04-27 18:35:57 +0600

    delete operation functionality added

--- index.js
+++ index.js
@@ -82,6 +82,13 @@ async function run() {
       const result = await touristSpotCollection.updateOne(filter, tourSpot, options);
       res.send(result)
     })
+
+    app.delete("/touristSpot/:id", async(req, res)=> {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await touristSpotCollection.deleteOne(query);
+      res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

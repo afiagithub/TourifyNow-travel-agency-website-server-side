4ca5991f1b331ae70c6cc31493519fcc778bb6ce
Afia Rahman <rahmanmou26@gmail.com>
2024-04-28 17:16:23 +0600

    read operation on country based tourist spot data done

--- index.js
+++ index.js
@@ -103,6 +103,18 @@ async function run() {
     const result = await countryCollection.insertOne(newCountry)
     res.send(result)
   })
+
+  app.get("/countrySpot", async (req, res) => {
+    const cursor = touristSpotCollection.find();
+    const result = await cursor.toArray();
+    res.send(result)
+  })
+
+  app.get("/countrySpot/:name", async (req, res) => {
+    const query = { country_Name: req.params.name };
+    const result = await touristSpotCollection.find(query).toArray();
+    res.send(result)
+  })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");

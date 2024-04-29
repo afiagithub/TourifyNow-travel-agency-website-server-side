3754c160420e66716eefe8a647bec8bfa946fcb7
Afia Rahman <rahmanmou26@gmail.com>
2024-04-29 18:56:35 +0600

    last commit

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const app = express()
 const port = process.env.PORT || 5000
 
 // middlewares
-app.use(cors())
+app.use(cors({
+  origin: ["http://localhost:5173", "https://tourify-now-app.web.app", "https://afia-assignment-10-server.vercel.app"],
+  credentials: true
+}))
 app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ctn12zm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -23,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const touristSpotCollection = client.db("touristSpotDB").collection("touristSpot");
     const countryCollection = client.db("touristSpotDB").collection("country");
@@ -34,8 +37,14 @@ async function run() {
       res.send(result)
     })
 
+    app.get("/countryList", async (req, res) => {
+      const cursor = countryCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+
     app.get("/sortedSpot", async (req, res) => {
-      const cursor = touristSpotCollection.find().sort({average_cost: 1});
+      const cursor = touristSpotCollection.find().sort({ average_cost: 1 });
       const result = await cursor.toArray();
       res.send(result)
     })
@@ -90,39 +99,26 @@ async function run() {
       res.send(result)
     })
 
-    app.delete("/touristSpot/:id", async(req, res)=> {
+    app.delete("/touristSpot/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)};
+      const query = { _id: new ObjectId(id) };
       const result = await touristSpotCollection.deleteOne(query);
       res.send(result)
-  })
+    })
 
-  app.get("/countryList", async (req, res) => {
-    const cursor = countryCollection.find();
-    const result = await cursor.toArray();
-    res.send(result)
-  })
+    app.post("/countryList", async (req, res) => {
+      const newCountry = req.body;
+      const result = await countryCollection.insertOne(newCountry)
+      res.send(result)
+    })
 
-  app.post("/countryList", async (req, res) => {
-    const newCountry = req.body;
-    console.log(newCountry)
-    const result = await countryCollection.insertOne(newCountry)
-    res.send(result)
-  })
-
-  app.get("/countrySpot", async (req, res) => {
-    const cursor = touristSpotCollection.find();
-    const result = await cursor.toArray();
-    res.send(result)
-  })
-
-  app.get("/countrySpot/:name", async (req, res) => {
-    const query = { country_Name: req.params.name };
-    const result = await touristSpotCollection.find(query).toArray();
-    res.send(result)
-  })
+    app.get("/countryList/:name", async (req, res) => {
+      const query = { country_Name: req.params.name };
+      const result = await touristSpotCollection.find(query).toArray();
+      res.send(result)
+    })
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
